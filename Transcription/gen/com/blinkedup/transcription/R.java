0df7fd50e278a3cfd7c3cdd0a7574aba4c63e2f8
Syd Samonte <seedlings_14@yahoo.com>
2015-01-14 08:09:38 +0800

    Updated Tabhost
    -Added images to drawable-hdpi
    -Added non existent ic_tabs
    -Updated existing ic_tabs
    -Modified Tabhost UI

--- Transcription/gen/com/blinkedup/transcription/R.java
+++ Transcription/gen/com/blinkedup/transcription/R.java
@@ -30,13 +30,24 @@ public final class R {
         public static final int home2=0x7f020005;
         public static final int ic_launcher=0x7f020006;
         public static final int ic_tab_about=0x7f020007;
-        public static final int ic_tab_contact=0x7f020008;
-        public static final int ic_tab_home=0x7f020009;
-        public static final int ldpi_normal=0x7f02000a;
-        public static final int ldpi_small=0x7f02000b;
-        public static final int login_button=0x7f02000c;
-        public static final int register_button=0x7f02000d;
-        public static final int shape=0x7f02000e;
+        public static final int ic_tab_account=0x7f020008;
+        public static final int ic_tab_contact=0x7f020009;
+        public static final int ic_tab_home=0x7f02000a;
+        public static final int ic_tab_info=0x7f02000b;
+        public static final int ic_tab_record=0x7f02000c;
+        public static final int ldpi_normal=0x7f02000d;
+        public static final int ldpi_small=0x7f02000e;
+        public static final int login_button=0x7f02000f;
+        public static final int register_button=0x7f020010;
+        public static final int shape=0x7f020011;
+        public static final int tab_cog=0x7f020012;
+        public static final int tab_cog_ch=0x7f020013;
+        public static final int tab_feed=0x7f020014;
+        public static final int tab_feed_ch=0x7f020015;
+        public static final int tab_info=0x7f020016;
+        public static final int tab_info_ch=0x7f020017;
+        public static final int tab_rec=0x7f020018;
+        public static final int tab_rec_ch=0x7f020019;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f080001;
--- Transcription/src/com/blinkedup/transcription/TabHostActivity.java
+++ Transcription/src/com/blinkedup/transcription/TabHostActivity.java
@@ -3,14 +3,18 @@ package com.blinkedup.transcription;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.TabHost;
 
 public class TabHostActivity extends TabActivity {
 	/** Called when the activity is first created. */
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		
 		setContentView(R.layout.activity_tab_host);
 	
 		Resources res = getResources(); // Resource object to get Drawables
@@ -21,32 +25,39 @@ public class TabHostActivity extends TabActivity {
 		// Create an Intent to launch an Activity for the tab (to be reused)
 		intent = new Intent().setClass(this, MainActivity.class);
 		spec = tabHost.newTabSpec("home")
-				.setIndicator("LOGIN", res.getDrawable(R.drawable.ic_tab_home))
+				.setIndicator("", res.getDrawable(R.drawable.ic_tab_home))
+				.setContent(intent);
+		tabHost.addTab(spec);
+		
+		intent = new Intent().setClass(this, AudioRecordingActivity.class);
+		spec = tabHost.newTabSpec("about")
+				.setIndicator("", res.getDrawable(R.drawable.ic_tab_record))
+				.setContent(intent);
+		tabHost.addTab(spec);
+		
+		intent = new Intent().setClass(this, RecordingsActivity.class);
+		spec = tabHost
+				.newTabSpec("contact")
+				.setIndicator("",
+						res.getDrawable(R.drawable.ic_tab_info))
+				.setContent(intent);
+		tabHost.addTab(spec);
+		
+		intent = new Intent().setClass(this, MainActivity.class);
+		spec = tabHost.newTabSpec("home")
+				.setIndicator("", res.getDrawable(R.drawable.ic_tab_account))
 				.setContent(intent);
 		tabHost.addTab(spec);
 
 		// Do the same for the other tabs
-
-		intent = new Intent().setClass(this, AudioRecordingActivity.class);
-		spec = tabHost.newTabSpec("about")
-				.setIndicator("RECORD", res.getDrawable(R.drawable.ic_tab_about))
-				.setContent(intent);
-		tabHost.addTab(spec);
-		
-		 
-		intent = new Intent().setClass(this, RecordingsActivity.class);
-		spec = tabHost
-				.newTabSpec("contact")
-				.setIndicator("RECORDINGS",
-						res.getDrawable(R.drawable.ic_tab_contact))
-				.setContent(intent);
-		tabHost.addTab(spec);
-		
 		
 		//set tab which one you want open first time 0 or 1 or 2
 		tabHost.setCurrentTab(0);
 		
-		
+		for (int i = 0; i < tabHost.getTabWidget().getTabCount(); i++) 
+		{
+			tabHost.getTabWidget().getChildAt(i).getLayoutParams().height = (int) (50 * this.getResources().getDisplayMetrics().density);
+			tabHost.getTabWidget().getChildAt(i).setBackgroundColor(Color.parseColor("#202020"));
+		}  
 	}
-	
 }
