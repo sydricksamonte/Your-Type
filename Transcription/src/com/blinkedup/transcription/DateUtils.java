2f4ddfc568c98b65a62af04c72964d8067ad1dad
Syd Samonte <seedlings_14@yahoo.com>
2015-01-22 14:39:03 +0800

    Added DateUtils class
    Important when fetching/constructing date related processes

---
+++ Transcription/src/com/blinkedup/transcription/DateUtils.java
@@ -0,0 +1,47 @@
+package com.blinkedup.transcription;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import android.util.Log;
+
+public class DateUtils {
+	public String convertStringToDate(String dateToConv){
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM:dd HH:mm:ss");
+		
+		Date convertedDate = new Date();
+		 String localTime = "";
+		try {
+			dateFormat.setTimeZone(TimeZone.getTimeZone("UTC")); 
+			convertedDate = dateFormat.parse(dateToConv);
+			
+			localTime = convertedToLocale(convertedDate);
+			
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Log.e("DA",e.getLocalizedMessage());
+		}
+		
+		return localTime;
+	}
+	
+	private String convertedToLocale(Date localeDate){
+		String converted;
+		Calendar calendar = new GregorianCalendar(TimeZone.getTimeZone("GMT"));
+
+	    DateFormat formatter = new SimpleDateFormat("dd MMM - hh:mm a");    
+	    formatter.setTimeZone(TimeZone.getDefault());  
+
+	    converted = formatter.format(localeDate.getTime());
+	    Log.e("OIdaax",converted);
+		return converted;
+	}
+
+}
