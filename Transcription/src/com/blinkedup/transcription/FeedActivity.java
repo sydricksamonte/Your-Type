c2d725be28a47c7b813febeae5be5f87e1869e51
Syd Samonte <seedlings_14@yahoo.com>
2015-01-23 15:08:54 +0800

    Applied colors based on status

--- Transcription/src/com/blinkedup/transcription/FeedActivity.java
+++ Transcription/src/com/blinkedup/transcription/FeedActivity.java
@@ -1,11 +1,19 @@
 package com.blinkedup.transcription;
 
+import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ShapeDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
@@ -16,6 +24,7 @@ import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,11 +34,15 @@ public class FeedActivity extends FragmentActivity implements LoaderCallbacks<Cu
 	ListView mListView;		
 	DateUtils dateFunc;
 	String dateFormatted = "";
+	Drawable img;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+        
+    	 //drawingImageView = (ImageView) findViewById(R.id.DrawingImageView);
+        
         dateFunc = new DateUtils();
         mListView = (ListView) findViewById(R.id.listview);        
         
@@ -48,23 +61,84 @@ public class FeedActivity extends FragmentActivity implements LoaderCallbacks<Cu
 		mAdapter.setViewBinder(new SimpleCursorAdapter.ViewBinder() { 
 			@Override public boolean setViewValue(View view, Cursor cursor, int column) 
 			{ 
+			
+			//	final ImageView drawingImageView = (ImageView) view.findViewById(R.id.DrawingImageView);
+				//File imgFile = new  File("/sdcard/Images/test_image.jpg");
+
+				//if(imgFile.exists()){
+				
+				   // Bitmap myBitmap = BitmapFactory.d
+
+				 //   ImageView myImage = (ImageView) findViewById(R.id.DrawingImageView);
+
+				 //   myImage.setImageResource(R.drawable.ic_stat_green);
+
+				//}
+				/* Bitmap.Config conf = Bitmap.Config.ARGB_8888;
+			        Bitmap bitmap = Bitmap.createBitmap(30, 30, conf);
+			        		
+			        Canvas canvas = new Canvas(bitmap);
+			        
+			     
+			     // Circle
+			        Paint paint = new Paint();
+			        paint.setColor(Color.GREEN);
+			        paint.setStyle(Paint.Style.STROKE);
+			        float x = 20;
+			        float y = 20;
+			        float radius = 5;
+			        canvas.drawCircle(x, y, radius, paint);
+			        
+			        
+			        drawingImageView.setImageBitmap(bitmap);
+			        */
+			        //////////////////////
+			        
+			        
+			        
 				if (column == 5) { 
 					TextView tv = (TextView) view;
+					//ImageView iv = new ImageView(this);
 					if (tv.getId() ==  R.id.recStatDesc){
 						 
 						String statVal = cursor.getString(cursor.getColumnIndex("_status"));
 						String statDesc;
 						if (statVal.equals("1")){
 							statDesc = "Uploaded - Awaiting Process";
+							img = getResources().getDrawable( R.drawable.colors_orange );
+							img.setBounds( 0, 0, 12, 12 );
+							tv.setCompoundDrawables( img, null, null, null );
 						}
 						else if (statVal.equals("2")){
 							statDesc = "Transcription Done";
+							img = getResources().getDrawable( R.drawable.colors_green );
+							img.setBounds( 0, 0, 12, 12 );
+							tv.setCompoundDrawables( img, null, null, null );
 						}
 						else{
 							statDesc = "Waiting for Upload";
+							img = getResources().getDrawable( R.drawable.colors_gray );
+							img.setBounds( 0, 0, 12, 12 );
+							tv.setCompoundDrawables( img, null, null, null );
 						}
 						tv.setText(statDesc);
-						return true; 
+						//tv.setBackgroundDrawable(background)(R.drawable.shape);
+						
+						
+						
+					//	ShapeDrawable shapeDrawable = (ShapeDrawable)tv.getBackground();
+						//shapeDrawable.getPaint().setColor(Color.parseColor("#800080"));
+						//   ImageView myImage = (ImageView) findViewById(R.id.DrawingImageView);
+
+						//    myImage.setImageResource(R.drawable.ic_stat_green);
+
+						//return true; 
+					//}
+					//else if (iv.getId() == R.id.DrawingImageView){
+						
+					       
+					        
+					        return true; 
 					}
 					else{
 						return false; 
