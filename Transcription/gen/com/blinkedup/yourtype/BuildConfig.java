60949239bb994f96d3d62bc2d6d21974e90b3c56
Syd Samonte <seedlings_14@yahoo.com>
2015-03-03 12:13:24 +0800

    Migration to com.blinkedup.yourtype 2

--- Transcription/gen/com/blinkedup/yourtype/BuildConfig.java
+++ Transcription/gen/com/blinkedup/yourtype/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.blinkedup.transcription;
+package com.blinkedup.yourtype;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
