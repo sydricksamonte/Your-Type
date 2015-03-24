a3a9c4f29da03dffb340702990bb8697a8f3ba22
Syd Samonte <seedlings_14@yahoo.com>
2015-03-24 14:23:36 +0800

    changed connection time out

--- Transcription/src/com/blinkedup/yourtype/Network.java
+++ Transcription/src/com/blinkedup/yourtype/Network.java
@@ -15,15 +15,16 @@ import android.util.Log;
 public class Network {
 public static boolean isNetworkAvailable(Context context){
 		
+	
 	    HttpGet httpGet = new HttpGet("http://www.google.com");
 	    HttpParams httpParameters = new BasicHttpParams();
 	    // Set the timeout in milliseconds until a connection is established.
 	    // The default value is zero, that means the timeout is not used.
-	    int timeoutConnection = 1000;
+	    int timeoutConnection = 2000;
 	    HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
 	    // Set the default socket timeout (SO_TIMEOUT)
 	    // in milliseconds which is the timeout for waiting for data.
-	    int timeoutSocket = 2000;
+	    int timeoutSocket = 3000;
 	    HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
 
 	    DefaultHttpClient httpClient = new DefaultHttpClient(httpParameters);
